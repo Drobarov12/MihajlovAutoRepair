fd22fbbc6e55a77ea7e73e0a7b4a3ac55a5fd5a4
Martin Drobarov <martin.drobarov@halicea.com>
2024-12-05 17:28:23 +0100

    dep | docker updates

--- mihajlov-auto-repair-frontend/src/services/api.js
+++ mihajlov-auto-repair-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_BASE_URL = "http://127.0.0.1:5105/api";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5105/api";
 
 // Fetch models from the backend
 export const fetchModels = async () => {
